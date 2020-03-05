a8955efd61523410d28860f467c8bf56c9335105
Julia Frizzie <jfrizzie98@comcast.net>
2020-03-05 09:14:18 -0500

    Schedule UI and Controller back button functionality

--- NetBeansProjects/IST361/src/ist361/ScheduleController.java
+++ NetBeansProjects/IST361/src/ist361/ScheduleController.java
@@ -1,14 +1,37 @@
 package ist361;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class ScheduleController implements Initializable{
     
     @FXML private Text actiontarget;
+    
+    @FXML protected void backButton(ActionEvent event) throws IOException{
+        try {
+            Stage stageOriginal = (Stage) actiontarget.getScene().getWindow();
+            stageOriginal.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("NavigationUI.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+
+            stage.setTitle("Main Menu");
+            stage.setScene(new Scene(root1));  
+            stage.show();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
